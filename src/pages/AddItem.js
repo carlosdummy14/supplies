5acf2d763d3d852fee7ee7cece5e1060946b813c
carlosdummy <carlosdummy14@gmail.com>
2021-09-10 14:48:18 -0500

    Add styles to AddItem

--- src/pages/AddItem.js
+++ src/pages/AddItem.js
@@ -9,31 +9,27 @@ import { addItem } from '../reducers/itemsReducer'
 const initialValues = {
   name: '',
   description: '',
-  stock: 1,
+  stock: 0,
   image: ''
 }
 
 const AddItemSchema = (items) => {
   return Yup.object().shape({
-    name: Yup.string().min(5).max(30, 'Too Long!').required('Required').trim().notOneOf(items, 'Item already exist'),
+    name: Yup.string()
+      .min(5)
+      .max(30, 'Too Long!')
+      .required('Required')
+      .trim()
+      .notOneOf(items, 'Item already exist'),
     description: Yup.string().min(10).max(40, 'Too Long!').required('Required'),
-    stock: Yup.number().positive().integer().max(99).required('Required'),
+    stock: Yup.number().integer().min(0).max(99).required('Required'),
     image: Yup.string()
   })
 }
 
-const styles = {
-  form: {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '500px',
-    margin: '0 auto'
-  }
-}
-
 const AddItem = () => {
   const dispatch = useDispatch()
-  const items = useSelector(({ items }) => items.map(item => item.name))
+  const items = useSelector(({ items }) => items.map((item) => item.name))
   const history = useHistory()
 
   const handleSubmit = (values) => {
@@ -54,64 +50,109 @@ const AddItem = () => {
   }
 
   const handleNameInput = (e, setValues) => {
-    setValues(
-      prevValues => (
-        {
-          ...prevValues,
-          [e.target.name]: e.target.value.toUpperCase()
-        }
-      )
-    )
+    setValues((prevValues) => ({
+      ...prevValues,
+      [e.target.name]: e.target.value.toUpperCase()
+    }))
   }
 
   return (
     <>
-      <h1 className='app-titles'>Add new Item</h1>
+      <h1 className="app-titles">Add new Item</h1>
       <Formik
         initialValues={initialValues}
         validationSchema={AddItemSchema(items)}
         onSubmit={handleSubmit}
       >
-        {
-          ({ errors, touched, setValues }) => (
-            <Form style={styles.form}>
-              <label htmlFor='name'>Name</label>
-              <Field
-                autoFocus='autofocus'
-                name='name'
-                type='text'
-                onChange={e => handleNameInput(e, setValues)}
-              />
-              {
-                errors.name && touched.name
-                  ? (<div>{errors.name}</div>)
-                  : null
-              }
-              <label htmlFor='description'>Description</label>
-              <Field name='description' type='text' />
-              {
-                errors.description && touched.description
-                  ? (<div>{errors.description}</div>)
-                  : null
-              }
-              <label htmlFor='stock'>Description</label>
-              <Field name='stock' type='number' />
-              {
-                errors.stock && touched.stock
-                  ? (<div>{errors.stock}</div>)
-                  : null
-              }
-              <label htmlFor='image'>Image</label>
-              <Field name='image' type='file' />
-              {
-                errors.image && touched.image
-                  ? (<div>{errors.image}</div>)
-                  : null
-              }
-              <button type='submit'>Submit</button>
-            </Form>
-          )
-      }
+        {({ errors, touched, setValues }) => (
+          <Form className="form form--maxwidth">
+            <div className="input-group">
+              <label className="form-label" htmlFor="name">
+                Name
+              </label>
+              <div className="form__field">
+                <Field
+                  autoFocus="autofocus"
+                  className="input input-lg"
+                  name="name"
+                  placeholder='Name of item'
+                  type="text"
+                  onChange={(e) => handleNameInput(e, setValues)}
+                />
+                {errors.name && touched.name
+                  ? (
+                  <span className="error">{errors.name}</span>
+                    )
+                  : (
+                  <span className="error" />
+                    )}
+              </div>
+            </div>
+
+            <div className="input-group">
+              <label className="form-label" htmlFor="description">
+                Description
+              </label>
+              <div className="form__field">
+                <Field
+                  className="input input-lg"
+                  name="description"
+                  placeholder='Description'
+                  type="text"
+                />
+                {errors.description && touched.description
+                  ? (
+                  <span className="error">{errors.description}</span>
+                    )
+                  : (
+                  <span className="error" />
+                    )}
+              </div>
+            </div>
+
+            <div className="input-group">
+              <label className="form-label" htmlFor="stock">
+                Initial Stock
+              </label>
+              <div className="form__field">
+                <Field
+                className="input input-lg"
+                name="stock" placeholder='Initial stock' type="number" />
+                {errors.stock && touched.stock
+                  ? (
+                  <span className="error">{errors.stock}</span>
+                    )
+                  : (
+                  <span className="error" />
+                    )}
+              </div>
+            </div>
+
+            <div className="input-group">
+              <label className="form-label" htmlFor="image">
+                Image
+              </label>
+              <div className="form__field">
+                <Field className="input input-lg" name="image" type="file" />
+                {errors.image && touched.image
+                  ? (
+                  <span className="error">{errors.image}</span>
+                    )
+                  : (
+                  <span className="error" />
+                    )}
+              </div>
+            </div>
+            <div className="form__buttons">
+              <button
+                className="button button-small button-small--nopadding"
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+          </Form>
+        )}
       </Formik>
     </>
   )
